aa19647885da93591fa1d24896014a082980c8c9
gauravtechies <gauravkamboj13121992@gmail.com>
2020-06-18 11:41:41 +0530

    named vs default exports

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import App from './components/App';
+//If we have to import export function then we will use curly braises otherwise if using export defauly then no need 
 
 ReactDom.render(
     <App/>,
